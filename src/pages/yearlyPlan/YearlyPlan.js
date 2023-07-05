b0b05d44f7113eae9fc8a0131581bed8b5183d38
ameen <ameen55668@gmail.com>
2023-07-05 15:00:36 +0500

    pages responsive

--- src/pages/yearlyPlan/YearlyPlan.js
+++ src/pages/yearlyPlan/YearlyPlan.js
@@ -1,21 +1,9 @@
 import style from './YearlyPlan.module.css'
 import SideBar from '../../components/sidebar/SideBar'
-import profileImg from '../../assets/images/hrprofile/Dropdown.svg'
-import circularuser from '../../assets/images/hrprofile/UserCircle.svg'
-import logout from '../../assets/images/hrprofile/Logout.svg'
-import { useState } from 'react'
-import hrStyle from '../HRProfile/HRProfile.module.css'
 import search from '../../assets/images/employees/Search.svg'
 import add from '../../assets/images/employees/Application Add.svg'
+import ProfileUser from '../../components/profileUser/ProfileUser'
 function YearlyPlan() {
-    const [isOpen, setIsOpen] = useState(false)
-    const toggler = () => {
-        if (isOpen) {
-            setIsOpen(false)
-        } else {
-            setIsOpen(true)
-        }
-    }
     let data = [
         '2023',
         '2023',
@@ -32,23 +20,8 @@ function YearlyPlan() {
             <div className={style.sidebar}>
                 <SideBar />
             </div>
+                <ProfileUser />
             <div className={style.subparent}>
-                <div className={hrStyle.userInfo}>
-                    <img src={profileImg} onClick={toggler} alt="" />
-                    {
-                        isOpen ?
-                            <div className={hrStyle.dropdown}>
-                                <div>
-                                    <img src={circularuser} alt="" />
-                                    <p>Profile</p>
-                                </div>
-                                <div>
-                                    <img src={logout} alt="" />
-                                    <p>Logout</p>
-                                </div>
-                            </div> : null
-                    }
-                </div>
                 <div className={style.searchbar}>
                     <div className={style.sec1}>
                         <img src={search} alt="" />
